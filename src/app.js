ecc2a4aabb4f24a96b82ce62b0a91b4f56b31ddb
Alejandra Bárcena <alejandrabarcena2024@gmail.com>
2025-07-06 22:53:59 +0000

    Generador de nombres de dominio funcional

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extensions = ['.com', '.net', '.us', '.io'];
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let p of pronoun) {
+  for (let a of adj) {
+    for (let n of noun) {
+      for (let ext of extensions) {
+        console.log(p + a + n + ext);
+      }
+    }
+  }
+}
